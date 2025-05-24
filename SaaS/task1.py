24f74f2ae1b3a2216988a367645bf23ae5240c4c
ajinky81d <149197890+ajinky81d@users.noreply.github.com>
2025-05-25 03:54:24 +0530

    Delete SaaS directory

--- SaaS/task1.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-
-file_path="C:\Users\Dell\Documents\SaaS\Database\customer_data_smart360.csv"
-output_file_path = "C:\Users\Dell\Documents\SaaS\ModifiedDatabase\customer_data_smart360.csv"
